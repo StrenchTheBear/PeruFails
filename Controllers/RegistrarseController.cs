b900c0c5f3b5ef031ec97354357004ad38671684
StrenchTheBear <acliv2904@yahoo.com>
2020-12-29 17:32:46 -0500

    Se agrego Registrarse controller model e index

---
+++ Controllers/RegistrarseController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PeruFails.Models;
+
+namespace PeruFails.Controllers
+{
+    public class RegistrarseController : Controller
+    {
+        private readonly ILogger<RegistrarseController> _logger;
+
+        public RegistrarseController(ILogger<RegistrarseController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
